d17a898a1dfb73d64eba328fc0a3e743c8bac645
nianqin <nianqin.vip@gmail.com>
2022-07-04 15:15:05 +0800

    update

---
+++ test/example/vve-statsvn-cli.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  ingorePaths: [
+    'eweb/**',
+    '**/dist/**',
+    '**/node_module/**'
+  ],
+  svnProjectPaths: [
+    { path: 'G:\\SvnWorkspaces\\20220629-inc-inc-emb-trunk-yr', alias: 'aaa' },
+  ] 
+  outDir: '.'
+}
